f3faec2ee1ee543fddded1a87d5e1fbb37f99b8a
大島秀顕 <b-i-hide>
2018-02-15 09:14:53 +0900

    [add]  初期設定のシェルを追加

---
+++ install/brew.sh
@@ -0,0 +1,46 @@
+# Update Homebrew
+brew update
+brew upgrade
+
+# General
+brew install zsh
+brew install peco
+
+# Version control system
+brew install git
+
+# For development
+## golang
+brew install go
+brew install hg
+
+## nodejs
+brew install nodebrew
+
+## ruby
+brew install rbenv
+brew install ruby-build
+
+# mysql
+brew install mycli
+
+# redis
+brew install redis
+
+
+########
+# Cask #
+########
+
+## Docker
+brew cask install docker
+
+#########
+# Fonts #
+#########
+
+# Add repository
+brew tap caskroom/fonts
+
+# install
+brew cask install font-source-code-pro-for-powerline
---
+++ install/symlinks.sh
@@ -0,0 +1,14 @@
+# zsh with prezto
+ln -snf "$BASEDIR"/zprezto "$HOME"/.zprezto
+ln -snf "$ZDOTDIR"/runcoms/zlogin "$HOME"/.zlogin
+ln -snf "$ZDOTDIR"/runcoms/zlogout "$HOME"/.zlogout
+ln -snf "$ZDOTDIR"/runcoms/zpreztorc "$HOME"/.zpreztorc
+ln -snf "$ZDOTDIR"/runcoms/zprofile "$HOME"/.zprofile
+ln -snf "$ZDOTDIR"/runcoms/zshenv "$HOME"/.zshenv
+ln -snf "$ZDOTDIR"/runcoms/zshrc "$HOME"/.zshrc
+
+# zsh
+ln -snf "$ZDOTDIR" "$HOME"/.zsh
+
+#vim
+ln -snf "$BASHDIR"/.vimrc "$HOME"/.vimrc
